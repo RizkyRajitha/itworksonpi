53e144e65d6cd2999849ae4c9b9eec8ff883d5a6
rizkyrajitha <rajithagunathilake@gmail.com>
2022-10-18 19:46:22 +0530

    added umami

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,22 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html>
-      <Head></Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script
+            async
+            defer
+            data-website-id="b2850162-f0af-418e-b21a-caa5f9952c5d"
+            src="https://umami-five.vercel.app/umami.js"
+          ></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
